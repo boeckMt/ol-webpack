be1928bf50db73253949bc6c9b252063ff215dcc
Mathias Boeck <Mathias.Boeck@dlr.de>
2020-07-28 14:43:28 +0200

    adjust webpack config with dev server and html/css output

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,68 @@
 const webpack = require('webpack');
+const { resolve, join } = require("path");
 
-module.exports = {
-  entry: './main.js',
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+var config = {
+  entry: './src/main.js',
   output: {
-    path: __dirname,
-    filename: 'bundle.js'
+    path: resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
   },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: '/',
+          },
+        },
+        // 'style-loader',
+        'css-loader'
+      ]
+    },
+    {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }, {
+      test: /\.html$/,
+      loader: 'html-loader'
+    }]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  }),
+  new MiniCssExtractPlugin({
+    filename: '[name].css',
+    chunkFilename: '[id].css',
+  })]
 };
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map';
+    config.target = "web";
+    /** webpack-dev-server configuration */
+    config.devServer = {
+      publicPath: '/',
+      contentBase: join(__dirname, './dist'),
+      watchContentBase: true,
+      compress: true
+    }
+  } else if (argv.mode === 'production') {
+
+  }
+
+  return config;
+};
+
